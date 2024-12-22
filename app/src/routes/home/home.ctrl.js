eb5def8fb2706f0e96d4a2f78e46ffdc67750f13
Jenny Jeon <jennyjeon@192-168-1-100.tpgi.com.au>
2024-12-22 12:42:09 +1100

    backend login function

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,42 @@
 "use strict";
 
-const home = (req, res) => {
-  res.render("home/index");
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+
+  login: (req, res) => {
+    res.render("home/login");
+  },
 };
 
-const login = (req, res) => {
-  res.render("home/login");
+const users = {
+  id: ["admin", "jenny", "jisu"],
+  pw: ["trcjj", "1234", "5678"],
+};
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      pw = req.body.pw;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.pw[idx] === pw) {
+        return res.json({
+          sucess: true,
+        });
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: "failed to login.",
+    });
+  },
 };
 
 module.exports = {
-  home,
-  login,
+  output,
+  process,
 };
