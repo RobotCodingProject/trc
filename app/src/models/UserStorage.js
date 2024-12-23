bd0f2a18cc5ea19b589864bdf8ba5e2adc77467d
Jenny Jeon <jennyjeon@192-168-1-100.tpgi.com.au>
2024-12-23 14:18:15 +1100

    login using fs module

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,21 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage {
-  static #users = {
-    id: ["admin", "jenny", "jisu"],
-    pw: ["trcjj", "1234", "5678"],
-    name: ["trc", "jenny", "jisu"],
-  };
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users);
+    const userInfo = usersKeys.reduce((newUsers, info) => {
+      newUsers[info] = users[info][idx];
+      return newUsers;
+    }, {});
+    return userInfo;
+  }
 
   static getUsers(...fields) {
-    const users = this.#users;
+    // const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnPropert(field)) {
         newUsers[field] = users[field];
@@ -19,19 +26,16 @@ class UserStorage {
   }
 
   static getUserInfo(id) {
-    const users = this.#users;
-    const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users);
-    const userInfo = usersKeys.reduce((newUsers, info) => {
-      newUsers[info] = users[info][idx];
-      return newUsers;
-    }, {});
-
-    return userInfo;
+    return fs
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error);
   }
 
   static save(userInfo) {
-    const users = this.#users;
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.pw.push(userInfo.pw);
