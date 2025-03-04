dcbf9ea38795719d2476c637acf3168a7fcea96e
Jisu Choi <Jisu.Choi-1@student.uts.edu.au>
2025-03-04 15:28:30 +1100

    update register page UI design

--- app/bin/www.js
+++ app/bin/www.js
@@ -2,7 +2,7 @@
 
 const app = require("../app");
 const logger = require("../src/config/logger");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 
 app.listen(PORT, () => {
   logger.info(`SERVER RUNNING ON PORT ${PORT}.`);
